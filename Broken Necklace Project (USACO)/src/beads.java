1473053cb635d6a231e01c8aea44519125b1e782
nbingo <nringach1@hwemail.com>
2015-10-20 14:38:53 -0700

    Began the project...

--- Broken Necklace Project (USACO)/src/beads.java
+++ Broken Necklace Project (USACO)/src/beads.java
@@ -1,4 +1,17 @@
+/*
+ID: nringac1
+LANG: JAVA
+TASK: beads
+*/
+import java.io.*;
+import java.util.*;
 
 public class beads {
 
+	public static void main (String [] args) throws IOException
+	{
+		BufferedReader read = new BufferedReader (new FileReader ("beads.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("beads.out")));
+		
+	}
 }
